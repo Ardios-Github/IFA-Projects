087b2d1d86a055d177bea2e48cdb7c2ca18c5ef3
Ardios <56349794+Ardios-Github@users.noreply.github.com>
2020-03-12 09:19:27 +0100

    Add files via upload

---
+++ Palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Palindrome
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+            Console.WriteLine("Entrez un mot ou une phrase...");
+            string phrase = Console.ReadLine();
+            int longueur = phrase.Length;
+
+            for (int i = 0; i < longueur / 2; i++)
+            {
+                if (phrase[i] != phrase[longueur - i - 1])
+                    Console.WriteLine("Ce n'est pas un palindrome.");
+                else {
+                    Console.WriteLine("C'est un palindrome !");
+                }
+                break;
+            }
+        }
+	}
+}
