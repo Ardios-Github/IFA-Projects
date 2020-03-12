65324a754061f1a26b55d88f25bc0b7058859eec
Ardios <56349794+Ardios-Github@users.noreply.github.com>
2020-03-12 09:21:39 +0100

    Add files via upload

---
+++ Etoiles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Etoiles
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int nbEtoiles = 0;
+			Console.WriteLine("Nb étoiles max ?");
+			nbEtoiles = Convert.ToInt16(Console.ReadLine());
+
+			for (int i = 0; i <= nbEtoiles; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					Console.Write("*");
+				}
+				Console.Write("\r\n");
+			}
+
+			for (int i = 0; i <= nbEtoiles; i++)
+			{
+				for (int j = nbEtoiles-1-i; j < i; j++)
+				{
+					Console.Write("*");
+				}
+				Console.Write("\r\n");
+			}
+		}
+	}
+}
